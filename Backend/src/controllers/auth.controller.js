0311a00517bdf01a4de2ef007e5638a800bf769f
Sumit-Kumar-Panigrahi <sumitkumarpanigrahi9278@gmail.com>
2026-07-19 13:24:01 +0530

    Update cookie settings for production

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -41,7 +41,12 @@ async function registerUserController(req,res){
         {expiresIn:"1d"}
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+})
 
     res.status(201).json({
         message:"User Registered Successfully",
@@ -83,7 +88,13 @@ async function loginUserController(req,res){
         {expiresIn:"1d"}
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token , {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+})
+
     res.status(200).json({
         message:"User Logged In Successfully",
         user:{
